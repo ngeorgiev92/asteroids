f8b338cbad816546728dea81ec5e671d884f565f
ngeorgiev92 <ngeorgiev92@gmail.com>
2025-03-22 23:17:10 +0000

    black window working, gitignore added

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+import time
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    screen_color = pygame.Color("black")
     print ("Starting Asteroids!")
-
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(screen_color)
+        pygame.display.flip()
+    pygame.quit()
 if __name__ == "__main__":
     main()
